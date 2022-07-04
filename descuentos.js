e98ec0aab8aad24c4081686b5e415e3a965749f5
Varamosf <varamosf@gmail.com>
2022-07-04 15:59:59 -0500

    Se agregan funciones con párrafo modificado para HTML desde JS.

--- descuentos.js
+++ descuentos.js
@@ -11,7 +11,17 @@ console.log({
 })
 
 function calcDiscount(originalPrice, discountPercentage) {
-  const discount = originalPrice * (discountPercentage / 100)
-  const finalPrice = originalPrice - discount
+  const discount = Number(originalPrice) * (Number(discountPercentage) / 100)
+  const finalPrice = Number(originalPrice) - Number(discount)
   return finalPrice
+}
+
+function calcFinalPrice() {
+  const IN_price = document.getElementById("IN_productPrice")
+  const VAL_price = Number(IN_price.value)
+  const IN_discountPercentage = document.getElementById("IN_productDiscountPercentage")
+  const VAL_discountPercentage = Number(IN_discountPercentage.value)
+  const calcFinalPrice_a = calcDiscount(VAL_price, VAL_discountPercentage)
+  const OUT_finalPrice = document.getElementById("OUT_productFinalPrice")
+  OUT_finalPrice.innerText = "The product final price is: $" + calcFinalPrice_a + " MXN."
 }
